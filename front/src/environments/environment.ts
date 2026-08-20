8f675f1d35d67f2b0c379787f67e7ac8d8e8062c
Vinicius do Canto <viniciustst7@gmail.com>
2026-08-20 10:36:20 -0300

    atualiza apiUrl para novo dominio

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://academia-ulbra.vercel.app'
+  apiUrl: 'https://ulbra-gym.vercel.app'
   // esse arquivo é usado automaticamente pelo "ionic build" em modo produção
   // (troca o environment.ts pelo environment.prod.ts) — aponta direto pro
   // backend hospedado no Vercel, não pro localhost
